8e00b6d6d91344863bbfefb29127076728c39c4e
asfrazao <Nena@2020>
2024-10-01 19:15:09 -0300

    Ajuste Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,19 @@
 import '../styles/globals.css'; // Estilos globais
 import '../styles/Header.css';  // Importe o CSS do Header aqui
 import '../styles/LoginForm.module.css'; // Estilos do LoginForm
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="theme-color" content="#000000" />
+                <meta name="description" content="Web site created using Next.js" />
+                <title>ForenseTools</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
