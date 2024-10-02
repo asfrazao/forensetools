e2180382fe243750adbe9495f1079d4195227da4
asfrazao <Nena@2020>
2024-10-02 08:13:35 -0300

    Debug no index 2

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react';
-import LoginForm from '../components/LoginForm';
-import HomePage from '../components/Home'; // Página do conteúdo principal
-import Header from '../components/Header'; // Header que será exibido após o login
+import dynamic from 'next/dynamic';
+
+const LoginForm = dynamic(() => import('../components/LoginForm'), { ssr: false });
+const HomePage = dynamic(() => import('../components/Home'), { ssr: false });
+const Header = dynamic(() => import('../components/Header'), { ssr: false });
 
 export default function Index() {
     // Estado para armazenar se o usuário está logado ou não
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [isClient, setIsClient] = useState(false);
 
-    // Verifica se estamos no cliente
     useEffect(() => {
         setIsClient(true);
         console.log('Componente Index foi renderizado');
@@ -16,7 +17,6 @@ export default function Index() {
 
     // Função para lidar com a autenticação
     const handleLogin = (username, password) => {
-        // Verifica se o usuário e a senha são "admin" e "teste"
         if (username === 'admin' && password === 'teste') {
             setIsAuthenticated(true);
         } else {
@@ -29,14 +29,12 @@ export default function Index() {
         setIsAuthenticated(false);
     };
 
-    // Conteúdo de fallback para renderização inicial
     if (!isClient) {
         return <div>Carregando...</div>;
     }
 
     return (
         <>
-            {/* Se o usuário estiver autenticado, exibe o Header e o conteúdo principal. Caso contrário, exibe a tela de login */}
             {isAuthenticated ? (
                 <>
                     <Header isAuthenticated={isAuthenticated} onLogout={handleLogout} />
