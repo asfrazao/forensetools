a1b9b5d682a9aaf0bf73d5dacdbde68611c1a601
asfrazao <Nena@2020>
2024-10-02 07:47:34 -0300

    Ajuste document JavaScript

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,13 @@
-// pages/_document.js
+// frontend/pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
-        <Html lang="pt-BR">
+        <Html lang="en">
             <Head>
                 <link rel="icon" href="/favicon.ico" />
+                <meta name="theme-color" content="#000000" />
+                <meta name="description" content="Web site created using Next.js" />
             </Head>
             <body>
             <Main />
@@ -14,3 +16,4 @@ export default function Document() {
         </Html>
     );
 }
+
